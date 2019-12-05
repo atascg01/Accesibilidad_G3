5193b2caf370356a3e82fa2d5b5c932cfcb036b7
atascg01 <atascg01@estudiantes.unileon.es>
2019-12-05 16:14:58 +0100

    Creada clase java para vista home, falta configuracion

--- app/src/main/java/com/example/myapplication/FragmentHome.java
+++ app/src/main/java/com/example/myapplication/FragmentHome.java
@@ -1,6 +1,12 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.navigation.NavController;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,14 +21,14 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import java.util.ArrayList;
+public class FragmentHome extends AppCompatActivity{
 
-public class MainActivity extends AppCompatActivity {
+    private Spinner spinnerOrdenar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_home);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -32,6 +38,10 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-    }
 
+        EditText asd = findViewById(R.id.editText2);
+        spinnerOrdenar = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adp = ArrayAdapter.createFromResource(this, R.array.ordenarSpinner, android.R.layout.simple_spinner_item);
+        spinnerOrdenar.setAdapter(adp);
+    }
 }
