424c41dc89b66acd999237b23755fb6284512ccf
atascg01 <atascg01@estudiantes.unileon.es>
2019-12-10 14:20:59 +0100

    Creada ventana Registrar, implementada logica en Registrar, Login, anyadido campo username a Perfil, cambios esteticos

--- app/src/main/java/com/example/buscahoteles/LoginFragment.kt
+++ app/src/main/java/com/example/buscahoteles/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.example.buscahoteles
 
 
+import android.graphics.Color
 import android.os.Bundle
 import android.provider.ContactsContract
 import androidx.fragment.app.Fragment
@@ -8,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import kotlinx.android.synthetic.main.fragment_login.*
 
 /**
@@ -22,9 +24,25 @@ class LoginFragment : Fragment() {
 
         val view : View = inflater!!.inflate(R.layout.fragment_login, container, false)
         val loginButton = view.findViewById<View>(R.id.buttonLogin) as Button
-
+        val registerButton = view.findViewById<View>(R.id.buttonRegister) as Button
+        val etUsername = view.findViewById<View>(R.id.etUsername_login) as EditText
+        val etPasswd = view.findViewById<View>(R.id.etPasswd_login) as EditText
         loginButton.setOnClickListener{
-            val fragment = SearchFragment()
+            if(etUsername.text.isNotEmpty() && etPasswd.text.isNotEmpty()){
+                val fragment = SearchFragment()
+                val fragmentManager = activity!!.supportFragmentManager
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+                fragmentTransaction.addToBackStack(null)
+                fragmentTransaction.commit()
+            }else{
+                etUsername.setHintTextColor(Color.RED)
+                etPasswd.setHintTextColor(Color.RED)
+            }
+
+        }
+        registerButton.setOnClickListener{
+            val fragment = RegisterFragment()
             val fragmentManager = activity!!.supportFragmentManager
             val fragmentTransaction = fragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.fragmentContainer, fragment)
---
+++ app/src/main/java/com/example/buscahoteles/RegisterFragment.kt
@@ -0,0 +1,71 @@
+package com.example.buscahoteles
+
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class RegisterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view: View = inflater!!.inflate(R.layout.fragment_register, container, false)
+        val buttonRegister = view.findViewById<View>(R.id.buttonRegister_register) as Button
+        val tvLogin = view.findViewById<View>(R.id.tvLogin_register) as TextView
+        val checkBoxTerms = view.findViewById<View>(R.id.checkBoxTerms) as CheckBox
+        val etName = view.findViewById<View>(R.id.etUsername_register) as EditText
+        val etMobile = view.findViewById<View>(R.id.etMobile_register) as EditText
+        val etEmail = view.findViewById<View>(R.id.etEmail_register) as EditText
+        val etPasswd = view.findViewById<View>(R.id.etPassword_register) as EditText
+        val tvForcedText = view.findViewById<View>(R.id.tvForcedText) as TextView
+        buttonRegister.setOnClickListener {
+            if(checkBoxTerms.isChecked && etName.text.isNotEmpty()&&
+            etMobile.text.isNotEmpty() && etEmail.text.isNotEmpty() && etPasswd.text.isNotEmpty()){
+                val fragment = LoginFragment()
+                val fragmentManager = activity!!.supportFragmentManager
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+                fragmentTransaction.addToBackStack(null)
+                fragmentTransaction.commit()
+            }else{
+                tvForcedText.setTextColor(Color.RED)
+                val components: Array<EditText> = arrayOf(etName, etMobile, etEmail, etPasswd)
+                for (i in components){
+                    if(i.text.isEmpty()){
+                        i.setHintTextColor(Color.RED)
+                    }else{
+                        i.setHintTextColor(Color.BLACK)
+                    }
+                }
+                if(!checkBoxTerms.isChecked){
+                    checkBoxTerms.setTextColor(Color.RED)
+                }else{
+                    checkBoxTerms.setTextColor(Color.BLACK)
+                }
+            }
+        }
+
+        tvLogin.setOnClickListener {
+            val fragment = LoginFragment()
+            val fragmentManager = activity!!.supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+        return view
+    }
+}
